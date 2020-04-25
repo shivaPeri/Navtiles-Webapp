768c35e02d9a2eae34e5452b1e3b606d749f903f
shivaPeri <57304890+shivaPeri@users.noreply.github.com>
2020-04-25 13:34:54 -0400

    Fixed preview tile issue. Changed image paths

--- js/design_script.js
+++ js/design_script.js
@@ -14,11 +14,13 @@ function createArray(rows, cols) {
 
 //
 
-var x = 50;
-var y = 250;
+var x = 100;
+var y = 200;
 var sq_len = 20;
 var tile_color = createArray(5);
 var tile_images = createArray(5);
+var p_height = 50;
+var tile_previews = createArray(5);
 var cur_color = 0;
 var rows;
 var cols;
@@ -26,7 +28,7 @@ var grid;
 var img;
 
 function setup() {
-  let img = createImg("https://firebasestorage.googleapis.com/v0/b/navtiles-bccb4.appspot.com/o/default_floorpan.jpeg?alt=media&token=6c871212-b3da-4859-bbb9-14e4abcc2913");
+  let img = createImg("./images/default_floorpan.jpeg");
   let canvas = createCanvas(600, 400);
 
   rows = Math.floor(height / sq_len);
@@ -38,40 +40,66 @@ function setup() {
   tile_color[3] = color(4, 214, 200); // blue
   tile_color[4] = color(199, 135, 255); //violet
 
-  tile_images[0] = createImg("https://firebasestorage.googleapis.com/v0/b/navtiles-bccb4.appspot.com/o/tile_previews%2Fbump_pattern.png?alt=media&token=f0e422c5-00ff-42e4-91df-455ff3924603");
+  tile_images[0] = createImg("./images/bump_pattern.png");
   tile_images[0].id("bump");
   tile_images[0].position(x + width, y);
   tile_images[0].size(AUTO, height);
   //tile_images[0].hide();
+  tile_previews[0] = createImg("./images/bump_pattern.png");
+  tile_previews[0].size(AUTO, p_height);
+  tile_previews[0].hide();
 
-  tile_images[1] = createImg("https://firebasestorage.googleapis.com/v0/b/navtiles-bccb4.appspot.com/o/tile_previews%2Fraised_bars.png?alt=media&token=bab2c4a7-729d-4403-b5d8-8cfb88f19ff0");
+  tile_images[1] = createImg("./images/raised_bars.png");
   tile_images[1].id("bars");
   tile_images[1].position(x + width, y);
   tile_images[1].size(AUTO, height);
   tile_images[1].hide();
+  tile_previews[1] = createImg("./images/raised_bars.png");
+  tile_previews[1].size(AUTO, p_height);
+  tile_previews[1].hide();
 
-  tile_images[2] = createImg("https://firebasestorage.googleapis.com/v0/b/navtiles-bccb4.appspot.com/o/tile_previews%2Fquestion_mark.png?alt=media&token=a8fa8b8f-4108-4d3f-87b2-557dae58d431");
+  tile_images[2] = createImg("./images/question_mark.png");
   tile_images[2].id("question");
   tile_images[2].position(x + width, y);
   tile_images[2].size(AUTO, height);
   tile_images[2].hide();
+  tile_previews[2] = createImg("./images/question_mark.png");
+  tile_previews[2].size(AUTO, p_height);
+  tile_previews[2].hide();
 
-  tile_images[3] = createImg("https://firebasestorage.googleapis.com/v0/b/navtiles-bccb4.appspot.com/o/tile_previews%2Farrow_1.png?alt=media&token=6ee9a627-a36e-467f-ae55-c4284c83f8f0");
+  tile_images[3] = createImg("./images/arrow_1.png");
   tile_images[3].id("arrow_1");
   tile_images[3].position(x + width, y);
   tile_images[3].size(AUTO, height);
   tile_images[3].hide();
+  tile_previews[3] = createImg("./images/arrow_1.png");
+  tile_previews[3].size(AUTO, p_height);
+  tile_previews[3].hide();
 
-  tile_images[4] = createImg("https://firebasestorage.googleapis.com/v0/b/navtiles-bccb4.appspot.com/o/tile_previews%2Farrow_2.png?alt=media&token=1f81b714-b5c5-4c39-b41a-f13ac2cdccd5");
+  tile_images[4] = createImg("./images/arrow_2.png");
   tile_images[4].id("arrow_2");
   tile_images[4].position(x + width, y);
   tile_images[4].size(AUTO, height);
   tile_images[4].hide();
+  tile_previews[4] = createImg("./images/arrow_2.png");
+  tile_previews[4].size(AUTO, p_height);
+  tile_previews[4].hide();
 
   canvas.position(x,y);
 
   img.position(x,y);
   img.size(width,height);
+
+  let x_off = 0;
+  let text_1 = createDiv("KEYBOARD SHORTCUTS:");
+  text_1.position(x + x_off, y+height + 20);
+  let text_2 = createDiv("left/right arrows --> edit tile scale");
+  text_2.position(x + x_off, y+height + 40);
+  let text_3 = createDiv("up/down arrows --> change tile type");
+  text_3.position(x + x_off, y+height + 60);
+
+  // let upload = createInput('');
+  // upload.input();
 }
 
 function draw_grid() {
@@ -99,10 +127,33 @@ function get_pos(a) {
   return Math.floor(a / sq_len) * sq_len;
 }
 
+function showPreview(){
+  var row = Math.floor(get_pos(mouseX) / sq_len);
+  var col = Math.floor(get_pos(mouseY) / sq_len);
+  if (0 <= row && row < cols && 0 <= col && col < rows){
+    if (grid[row][col] != 0) {
+      for (let i = 0; i < 5; i++){
+        tile_previews[i].hide();
+      }
+      var x_pos = x + get_pos(mouseX) + sq_len;
+      var y_pos = y + get_pos(mouseY) - p_height;
+      tile_previews[grid[row][col]-1].position(x_pos, y_pos);
+      tile_previews[grid[row][col]-1].show();
+    }
+    else {
+      for (let i = 0; i < 5; i++){
+        tile_previews[i].hide();
+      }
+    }
+  }
+}
+
 function draw_cursor() {
   stroke(tile_color[cur_color]);
   noFill();
   rect(get_pos(mouseX), get_pos(mouseY), sq_len, sq_len);
+
+  showPreview();
 }
 
 function changeColor() {
@@ -115,9 +166,9 @@ function changeColor() {
   }
 }
 
-function mouseClicked(){changeColor()}
+function mouseClicked(){changeColor();}
 
-function mouseDragged(){changeColor()}
+//function mouseDragged(){changeColor();}
 
 function keyPressed() {
   if (keyCode === LEFT_ARROW && sq_len > 2) {
@@ -140,6 +191,13 @@ function keyPressed() {
     cur_color %= 5;
     tile_images[cur_color].show();
   }
+  if (keyCode === DOWN_ARROW) {
+    tile_images[cur_color].hide();
+    if (cur_color == 0) {cur_color = 4;}
+    else{cur_color--;}
+    cur_color %= 5;
+    tile_images[cur_color].show();
+  }
 
 }
 
@@ -147,7 +205,7 @@ function draw() {
   draw_grid();
   draw_cursor();
   noFill();
-  stroke(0);
+  stroke(tile_color[cur_color]);
   strokeWeight(5);
   rect(0,0,width, height);
 }
