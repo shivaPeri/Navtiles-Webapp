56c7fdbd44acf774ead628b970ba289c39d683b7
shivaPeri <57304890+shivaPeri@users.noreply.github.com>
2020-04-28 01:18:37 -0400

    revised interface; stored values in dictionary instead of array

--- js/design_script.js
+++ js/design_script.js
@@ -14,36 +14,47 @@ function createArray(rows, cols) {
 }
 //
 
+// position and scale
 var x = 100;
-var y = 200;
+var y = 250;
 var sq_len = 20;
+
+// data
 var tile_color = createArray(5);
 var tile_images = createArray(5);
 var tile_default_message = createArray(5);
-var tile_messages = {};
 var p_height = 50;
 var tile_previews = createArray(5);
 var cur_color = 0;
 var rows, cols, grid;
+var edit_mode = false;
+
+// interface
+let scale, clear_btn, tile_type, submit;
 let img, new_img, canvas, upload;
 let voice = new p5.Speech();
 let inp, message;
-var edit_mode = false;
-var toggle_sound = true;
 var cursor_row = 0;
 var cursor_col = 0;
 
 function setup() {
   img = createImg("./images/default_floorpan.jpeg");
   canvas = createCanvas(600, 400);
+  canvas.mouseClicked(changeColor);
+  canvas.doubleClicked(editAudio);
+
+  let y_off = 20
 
   inp = createInput();
-  inp.position(x + width, y + height);
+  inp.position(x + width, y + height + y_off);
   inp.input(handleInput);
   inp.hide();
-  //save = createButton('Save changes');
+  submit = createButton('submit');
+  submit.position(x + width, y + height + 2*y_off);
+  submit.mousePressed(toggleEdit);
+  submit.hide();
   message = createDiv('Tile Message: ');
-  message.position(x + width, y + height + 5);
+  message.position(x + width, y + height + y_off);
 
   tile_default_message[0] = 'CAUTION';
   tile_default_message[1] = 'FOLLOW';
@@ -53,7 +64,22 @@ function setup() {
 
   rows = Math.floor(height / sq_len);
   cols = Math.floor(width / sq_len);
-  grid = createArray(cols, rows);
+  grid = {};
+  scale = createSlider(10, 100, 20);
+  scale.position(x + 80, y + height + y_off);
+
+  clear_btn = createButton('Clear Grid');
+  clear_btn.mousePressed(clearGrid);
+  clear_btn.position(x + width - 100, y + height + y_off);
+
+  tile_type = createSelect();
+  tile_type.position(x + 300, y + height + y_off);
+  tile_type.option('Bump Pattern');
+  tile_type.option('Raised Bars');
+  tile_type.option('Question Mark');
+  tile_type.option('Arrow 1');
+  tile_type.option('Arrow 2');
+
   tile_color[0] = color(235, 64, 52); // red
   tile_color[1] = color(255, 221, 28); // yellow
   tile_color[2] = color(142, 235, 49); // green
@@ -92,27 +118,23 @@ function setup() {
 
   createPreviews();
   canvas.position(x,y);
-
   img.position(x,y);
   img.size(width,height);
 
   helpInfo();
   upload = createFileInput(handleFile);
-  upload.position(x, y- 50);
+  let text = createDiv('Upload a custom floor plan: ');
+  text.position(x, y - 20);
+  upload.position(x + 200, y - 20);
 }
 
 function helpInfo(){
-  let x_off = 0;
-  let text_1 = createDiv("KEYBOARD SHORTCUTS:");
-  text_1.position(x + x_off, y+height + 20);
-  let text_2 = createDiv("LEFT/RIGHT arrows --> edit tile scale (also clears grid)");
-  text_2.position(x + x_off, y+height + 40);
-  let text_3 = createDiv("UP/DOWN arrows --> change tile type");
-  text_3.position(x + x_off, y+height + 60);
-  let text_4 = createDiv("ESCAPE --> toggle edit mode*");
-  text_4.position(x + x_off, y+height + 80);
-  let text_5 = createDiv("*in edit mode, arrow keys edit cursor postion");
-  text_5.position(x + x_off, y+height + 100);
+  let text_1 = createDiv('Grid Scale: ');
+  text_1.position(x , y + height + 20);
+  let text_2 = createDiv('Tile: ');
+  text_2.position(x + 260, y + height + 20);
+  let text_3 = createDiv('**Double-click on a tile to edit its audio');
+  text_3.position(x, y + height + 50);
 }
 
 function createPreviews(){
@@ -145,18 +167,42 @@ function handleFile(file) {
     new_img.size(width,height);
     img = new_img;
     canvas = createCanvas(600, 400);
-    grid = createArray(cols, rows);
+    grid = {};
     createPreviews();
   } else {
     new_img = null;
   }
 }
 
-function handleInput(){
-  var key = toKey(cursor_row, cursor_col);
-  tile_messages[key] = inp.value();
+function toggleEdit() {
+  edit_mode = !edit_mode;
+  message.show();
+  inp.hide();
+  submit.hide();
 }
 
+function handleInput(){
+  var key = toKey(cursor_row, cursor_col);
+  grid[key][1] = inp.value();
+}
+
+function editAudio(){
+  var row = Math.floor(get_pos(mouseX) / sq_len);
+  var col = Math.floor(get_pos(mouseY) / sq_len);
+  cursor_row = row;
+  cursor_col = col;
+  print(cursor_row, cursor_col);
+  var key = toKey(row, col);
+  if (grid[key] != undefined) {
+    edit_mode = !edit_mode;
+    message.hide();
+    inp.show();
+    inp.value(grid[key][1]);
+    submit.show();
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 function draw_grid() {
   for (let i = 0; i < rows; i++) {
     stroke(245);
@@ -168,34 +214,38 @@ function draw_grid() {
     strokeWeight(1);
     line((i + 1) * sq_len, 0, (i + 1) * sq_len, height);
   }
-  for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < cols; c++) {
-      if (grid[c][r] != 0) {
-        fill(tile_color[grid[c][r] - 1]);
-        rect(c * sq_len, r * sq_len, sq_len, sq_len);
-      }
-    }
+  for(var key in grid) {
+    var v = grid[key];
+    fill(tile_color[v[0]]);
+    var pos = fromKey(key);
+    rect(pos[0] * sq_len, pos[1] * sq_len, sq_len, sq_len);
   }
+}
+
+function clearGrid(){
+  grid = {};
 }
 
 function get_pos(a) {
   return Math.floor(a / sq_len) * sq_len;
 }
 
+////////////////////////////////////////////////////////////////////////////////
 function showPreview(){
   var row = Math.floor(get_pos(mouseX) / sq_len);
   var col = Math.floor(get_pos(mouseY) / sq_len);
+  var key = toKey(row, col);
   if (0 <= row && row < cols && 0 <= col && col < rows){
-    if (grid[row][col] != 0) {
+    if (grid[key] != undefined) {
       for (let i = 0; i < 5; i++){
         tile_previews[i].hide();
       }
       var x_pos = x + get_pos(mouseX) + sq_len;
       var y_pos = y + get_pos(mouseY) - p_height;
-      tile_previews[grid[row][col]-1].position(x_pos, y_pos);
-      tile_previews[grid[row][col]-1].show();
+      tile_previews[grid[key][0]].position(x_pos, y_pos);
+      tile_previews[grid[key][0]].show();
       var key = toKey(row, col);
-      message.html(concat('Tile Message: ', tile_messages[key]));
+      message.html(concat('Tile Message: ', grid[key][1]));
     }
     else {
       for (let i = 0; i < 5; i++){
@@ -206,6 +256,7 @@ function showPreview(){
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////
 function draw_cursor() {
   stroke(tile_color[cur_color]);
   noFill();
@@ -215,7 +266,7 @@ function draw_cursor() {
   var row = Math.floor(get_pos(mouseX) / sq_len);
   var col = Math.floor(get_pos(mouseY) / sq_len);
   var key = toKey(row, col);
-  if (tile_messages[key] != undefined) voice.speak(tile_messages[key]);
+  if (grid[key] != undefined) voice.speak(grid[key][1]);
   else voice.stop();
 }
 
@@ -223,106 +274,60 @@ function toKey(row, col){
   return concat(concat(str(row), ','), str(col));
 }
 
-function draw_edit_mode(){
-  stroke(0);
-  noFill();
-  strokeWeight(3);
-  rect(cursor_row * sq_len, cursor_col * sq_len, sq_len, sq_len);
+function fromKey(key){
+  var res = key.split(',');
+  return [int(res[0]), int(res[1])];
 }
 
+////////////////////////////////////////////////////////////////////////////////
 function changeColor() {
   var row = Math.floor(get_pos(mouseX) / sq_len);
   var col = Math.floor(get_pos(mouseY) / sq_len);
   var key = toKey(row, col);
-  if (grid[row][col] != cur_color + 1) {
-    grid[row][col] = cur_color + 1;
-    tile_messages[key] = tile_default_message[cur_color];
-  }
-  else {
-    grid[row][col] = 0;
-    delete(tile_messages[key]);
-    clear();
-  }
+  grid[key] = [cur_color, tile_default_message[cur_color]];
 }
 
-function mouseClicked(){
-  if (!edit_mode) changeColor();
-  //print(tile_messages);
-}
+// function keyPressed() {
+//   if (keyCode === LEFT_ARROW) {
+//     tile_images[cur_color].hide();
+//     cur_color++;
+//     cur_color %= 5;
+//     tile_images[cur_color].show();
+//   }
+//   if (keyCode === RIGHT_ARROW) {
+//     tile_images[cur_color].hide();
+//     if (cur_color == 0) {cur_color = 4;}
+//     else{cur_color--;}
+//     cur_color %= 5;
+//     tile_images[cur_color].show();
+//   }
+// }
 
-function keyPressed() {
-  if (keyCode == ESCAPE) {
-    if (edit_mode){
-      inp.hide();
-      message.show();
-    }
-    else {
-      inp.show();
-      message.hide();
-    }
-    edit_mode = !edit_mode;
-    clear();
-  }
-  if (!edit_mode){
-    if (keyCode === LEFT_ARROW && sq_len > 2) {
-      sq_len--;
-      rows = Math.floor(height / sq_len);
-      cols = Math.floor(width / sq_len);
-      grid = createArray(cols, rows);
-      clear();
-    } else if (keyCode === RIGHT_ARROW) {
-      sq_len++;
-      rows = Math.floor(height / sq_len);
-      cols = Math.floor(width / sq_len);
-      grid = createArray(cols, rows);
-      clear();
-    }
-
-    if (keyCode === UP_ARROW) {
-      tile_images[cur_color].hide();
-      cur_color++;
-      cur_color %= 5;
-      tile_images[cur_color].show();
-    }
-    if (keyCode === DOWN_ARROW) {
-      tile_images[cur_color].hide();
-      if (cur_color == 0) {cur_color = 4;}
-      else{cur_color--;}
-      cur_color %= 5;
-      tile_images[cur_color].show();
-    }
-  }
-  else {
-    clear();
-    if (keyCode === LEFT_ARROW && cursor_row > 0) {
-      cursor_row--;
-    }
-    if (keyCode === RIGHT_ARROW && cursor_row < cols-1) {
-      cursor_row++;
-    }
-    if (keyCode === UP_ARROW && cursor_col > 0) {
-      cursor_col--;
-    }
-    if (keyCode === DOWN_ARROW && cursor_col < rows-1) {
-      cursor_col++;
-    }
-    var k = toKey(cursor_row, cursor_col);
-    if (tile_messages[k] != undefined) inp.value(tile_messages[k]);
-    else inp.value('');
-  }
+function updateColorVal(){
+  let prev = cur_color;
+  if (tile_type.value() == 'Bump Pattern') cur_color = 0;
+  if (tile_type.value() == 'Raised Bars') cur_color = 1;
+  if (tile_type.value() == 'Question Mark') cur_color = 2;
+  if (tile_type.value() == 'Arrow 1') cur_color = 3;
+  if (tile_type.value() == 'Arrow 2') cur_color = 4;
+  tile_images[prev].hide();
+  tile_images[cur_color].show();
 }
 
 function draw() {
+  if (!edit_mode)
+  {sq_len = scale.value();
+  rows = Math.floor(height / sq_len);
+  cols = Math.floor(width / sq_len);
+  clear();
+  updateColorVal();
   draw_grid();
-  if (!edit_mode){
-    draw_cursor();
-    stroke(0);
-  }
-  else {
-    draw_edit_mode();
-    stroke(255, 0, 0);
-  }
+  draw_cursor();
+  stroke(0);
   noFill();
   strokeWeight(5);
-  rect(0,0,width, height);
+  rect(0,0,width, height);}
+  else {
+    voice.stop();
+  }
 }
